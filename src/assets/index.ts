19cbe045ec1982354595860897190c75ba4de658
raxit <raxit988@gmail.com>
2025-03-22 23:48:47 +0530

    feat: :sparkles: resources
    added all the required resources

---
+++ src/assets/index.ts
@@ -0,0 +1,14 @@
+export enum Images {
+  FIRST = require("./1.jpg"),
+  SECOND = require("./2.jpg"),
+  THIRD = require("./3.jpg"),
+  FOURTH = require("./4.jpg"),
+  FIVE = require("./5.jpg"),
+  SIX = require("./6.jpg"),
+  SEVEN = require("./7.jpg"),
+  EIGHT = require("./8.jpg"),
+  NINE = require("./9.jpg"),
+  TEN = require("./10.jpg"),
+  GREEN = require("./green.jpg"),
+  LOGO = require("./Logo.jpg"),
+}
